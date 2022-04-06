6ff9d5f12c0527a22601a9f39cf2af300445f191
Prottoy Modak <prottoy.modak08@gmail.com>
2022-04-06 18:23:59 +0600

    Frame structure created

--- CollageManagementSystem.py
+++ CollageManagementSystem.py
@@ -2,8 +2,11 @@ from tkinter import*
 
 # Class Define
 class Student:
-    
-
+        def __init__(self, root):
+            self.root = root
+            self.root.title("Collage Management System")
+            self.root.geometry("1350x750+0+0")
+            self.root.configure(background="#393E46")
 
 
 
